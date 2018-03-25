706cb8e60e04ad6976a433b44eb6b3c5b6fd7526
Osamah <oshareef84@gmail.com>
2018-03-25 12:54:02 -0500

    Update POM comments

--- src/test/java/edu/matc/persistence/TestGoogleMapsApi.java
+++ src/test/java/edu/matc/persistence/TestGoogleMapsApi.java
@@ -18,12 +18,14 @@ public class TestGoogleMapsApi {
     @Test
     public void testGoogleApiJSON() throws Exception {
         Client client = ClientBuilder.newClient();
-        WebTarget target =
-                client.target("http://maps.googleapis.com/maps/api/geocode/json?" +
+        WebTarget target = client.target("http://maps.googleapis.com/maps/api/geocode/json?" +
                         "address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=false");
+        // JSON big file that we get from the service
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
 
+        // mapper part of Jackson
         ObjectMapper mapper = new ObjectMapper();
+        // readValue() to read the response and particular class we want to parse into.
         Results results = mapper.readValue(response, Results.class);
         ResultsItem item = results.getResults().get(0);
 
